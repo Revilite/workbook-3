1b164ff2f30b779c8c5c52c8c4c688d761d90afc
Jacob Lockhart <procoder165@gmail.com>
2024-10-09 15:53:29 -0400

    adding comments

--- payroll-calculator/src/main/java/com/pluralsight/payrollcalc/PayrollCalculator.java
+++ payroll-calculator/src/main/java/com/pluralsight/payrollcalc/PayrollCalculator.java
@@ -12,7 +12,6 @@ public class PayrollCalculator {
 
 
         String input = bufReader.readLine();
-
         while ((input = bufReader.readLine()) != null) {
             String[] values = input.split("[|]");
             Employee employee = new Employee(Integer.parseInt(values[0]), values[1], Float.parseFloat(values[2]), Float.parseFloat(values[3]));
@@ -27,18 +26,19 @@ public class PayrollCalculator {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
+        //User input and class declarations
         System.out.println("Enter the name of the file employee file to process: ");
         String employeeFile = scan.nextLine();
         System.out.println("Enter the name of the payroll file to create: ");
         String payrollFile = scan.nextLine();
-
         BufferedReader buffRead = new BufferedReader(new FileReader("./src/main/resources/DataFiles/" + employeeFile));
         BufferedWriter buffWrite = new BufferedWriter(new FileWriter("./src/main/resources/DataFiles/" + payrollFile));
 
-
+        //For Heading
         String input = buffRead.readLine();
         String[] parts = input.split("[|]");
-        buffWrite.write(String.format("%s|%s|Gross-Payment \n", parts[0], parts[1], parts[2]));
+        buffWrite.write(String.format("%s|%s|Gross-Payment \n", parts[0], parts[1]));
+        //adding
         while((input = buffRead.readLine()) != null){
             parts = input.split("[|]");
             String writer = String.format("%s|%s|%.2f\n", parts[0], parts[1], (Double.parseDouble(parts[2]) * Double.parseDouble(parts[3])));
