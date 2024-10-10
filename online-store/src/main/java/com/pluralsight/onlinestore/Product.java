55ff2c06bed67c95892e860d33ed09b6ccfbd382
Jacob Lockhart <procoder165@gmail.com>
2024-10-10 10:37:11 -0400

    added hashmaps and continued working on searching

--- online-store/src/main/java/com/pluralsight/onlinestore/Product.java
+++ online-store/src/main/java/com/pluralsight/onlinestore/Product.java
@@ -4,13 +4,20 @@ public class Product {
     private String sku;
     private String productName;
     private double price;
-    private String electronics;
+    private String department;
 
-    public Product(String sku, String productName, double price, String electronics) {
+    //String colors
+    String reset = "\033[1;0m";
+    String blueForeground = "\033[1;34m";
+    String magentaForeground = "\033[1;35m";
+    String redForeground = "\033[1;31m";
+    String greenForeground = "\033[1;32m";
+
+    public Product(String sku, String productName, double price, String department) {
         this.sku = sku;
         this.productName = productName;
         this.price = price;
-        this.electronics = electronics;
+        this.department = department;
     }
 
     public void setSku(String sku) {
@@ -25,8 +32,8 @@ public class Product {
         this.price = price;
     }
 
-    public void setElectronics(String electronics) {
-        this.electronics = electronics;
+    public void setElectronics(String department) {
+        this.department = department;
     }
 
     public String getSku() {
@@ -42,6 +49,17 @@ public class Product {
     }
 
     public String getElectronics() {
-        return electronics;
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(redForeground).append("Product name: ").append(this.productName);
+        sb.append(blueForeground).append(" Price: $").append(this.price);
+        sb.append(greenForeground).append(" Department: ").append(this.department);
+        sb.append(magentaForeground).append(" SKU: ").append(this.sku);
+        sb.append(reset);
+        return sb.toString();
     }
 }
--- online-store/src/main/java/com/pluralsight/onlinestore/StoreFront.java
+++ online-store/src/main/java/com/pluralsight/onlinestore/StoreFront.java
@@ -3,31 +3,99 @@ package com.pluralsight.onlinestore;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class StoreFront {
-    public static ArrayList fillStore() throws IOException {
+    public static HashMap<String, Product> fillStore() throws IOException {
+        //initializing
         BufferedReader buffRead;
         buffRead = new BufferedReader(new FileReader("./src/main/resources/DataFiles/products.csv"));
-        ArrayList<Product> inventory = new ArrayList<>();
+        HashMap<String, Product> inventory = new HashMap();
 
+        //Skips heading line and adds items to array list
         String input = buffRead.readLine();
-        while ((input = buffRead.readLine()) != null){
+        while ((input = buffRead.readLine()) != null) {
             String[] value = input.split("[|]");
             Product item = new Product(value[0], value[1], Double.parseDouble(value[2]), value[3]);
-            inventory.add(item);
+            inventory.put(value[0], item);
         }
+
         return inventory;
     }
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(fillStore());
-        } catch (IOException e) {
-            System.out.println("Failed to fill store");
+    public static void displayAllProducts(HashMap<String, Product> inventory, HashMap<String, Product> cart) {
+        Scanner scan = new Scanner(System.in);
+        String productPageChoice = "";
+
+        for (String key : inventory.keySet()) {
+            System.out.println(inventory.get(key));
         }
 
+        while (!productPageChoice.equals("b")) {
+            System.out.println("""
+                    Search for a item        (S)
+                    Add an item to your cart (C)
+                    Go back to the main page (B)
+                    """);
+            productPageChoice = scan.nextLine().toLowerCase();
+            switch (productPageChoice) {
+                case "s": {
+                    //Gets the item from inventory
+                    System.out.println("What is the SKU of the item your looking for?");
+                    String userSKU = scan.nextLine().toUpperCase();
 
 
+                    //Gets user input if they want to save this item or not
+                    System.out.println("Would you like to add this item to your cart? (Y/N)");
+                    String addItemFromSearch = scan.nextLine().toLowerCase();
+
+                    //Adds item and removes from inventory
+                    if (addItemFromSearch.equals("y")) {
+                        cart.put(userSKU, inventory.get(userSKU));
+                        inventory.remove(userSKU);
+                    } else if (addItemFromSearch.equals("n")) {
+                        break;
+                    } else {
+                        System.out.println("That isn't a choice");
+                    }
+                }
+            }
+        }
     }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        HashMap<String, Product> inventory = new HashMap<>();
+        HashMap<String, Product> cart = new HashMap<>();
+        try {
+            inventory = fillStore();
+        } catch (IOException e) {
+            System.out.println("Failed to fill store!");
+        }
+        System.out.println("""
+                    Welcome to my online store!
+                """);
+
+        String userIsDone = "";
+        while (!userIsDone.equals("e")) {
+            System.out.println("""
+                    Display All Products (P)
+                    Display Your Cart    (C)
+                    Exit Program         (E)
+                    """);
+
+            userIsDone = scan.nextLine().toLowerCase();
+            switch (userIsDone) {
+                case "p": {
+                    displayAllProducts(inventory, cart);
+                }
+            }
+        }
+    }
+
+
 }
+
